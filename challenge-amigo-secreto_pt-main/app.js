f04faadce8ce839f7ceb7b19012ff2d8a9df412b
Jeniffer Andrade <devcontactar@gmail.com>
2025-02-08 20:18:15 -0300

    Update app.js

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -10,44 +10,46 @@ function adicionarAmigo() {
     alert("Por favor, insira um nome.");
     return;
   }
-}
 
-amigos.push(nome);
+  amigos.push(nome);
   atualizarListaAmigos();
   inputNome.value = "";
-  function atualizarListaAmigos() {
-    const lista = document.getElementById("listaAmigos");
-    lista.innerHTML = "";
-    amigos.forEach((amigo, index) => {
-      lista.innerHTML += `<li class="name-item">${index + 1}. ${amigo}</li>`;
-    });
+}
+
+function atualizarListaAmigos() {
+  const lista = document.getElementById("listaAmigos");
+  lista.innerHTML = "";
+  amigos.forEach((amigo, index) => {
+    lista.innerHTML += `<li class="name-item">${index + 1}. ${amigo}</li>`;
+  });
+}
+
+function sortearAmigo() {
+  if (amigos.length === 0) {
+    alert("Nenhum amigo disponível para sortear.");
+    return;
   }
 
-  function sortearAmigo() {
-    if (amigos.length === 0) {
-      alert("Nenhum amigo disponível para sortear.");
-      return;
-    }
-  
-    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
-    const amigoSorteado = amigos[indiceAleatorio];
-    
-    const resultadoLista = document.getElementById("resultado");
-    resultadoLista.innerHTML = `<li class="result-item">Amigo Secreto Sorteado: <strong>${amigoSorteado}</strong></li>`;
-  }
-  
-  function limparLista() {
-    amigos = [];
-    atualizarListaAmigos();
-    document.getElementById("resultado").innerHTML = "";
-  }
-  
-  document.addEventListener("DOMContentLoaded", () => {
-    const buttonContainer = document.querySelector(".button-container");
-    const clearButton = document.createElement("button");
-    clearButton.textContent = "Limpar Lista";
-    clearButton.classList.add("button-clear");
-    clearButton.onclick = limparLista;
-    buttonContainer.appendChild(clearButton);
-  });
+  const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+  const amigoSorteado = amigos[indiceAleatorio];
+
+  const resultadoLista = document.getElementById("resultado");
+  resultadoLista.innerHTML = `<li class="result-item">Amigo Secreto Sorteado: <strong>${amigoSorteado}</strong></li>`;
+}
+
+function limparLista() {
+  amigos = [];
+  atualizarListaAmigos();
+  document.getElementById("resultado").innerHTML = "";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const buttonContainer = document.querySelector(".button-container");
+  const clearButton = document.createElement("button");
+  clearButton.textContent = "Limpar Lista";
+  clearButton.classList.add("button-clear");
+  clearButton.onclick = limparLista;
+  buttonContainer.appendChild(clearButton);
+});
+
   
