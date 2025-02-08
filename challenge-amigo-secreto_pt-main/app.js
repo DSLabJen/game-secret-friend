cb3882548182c2825bb3bdda7e26e0aaf208e16d
Jeniffer Andrade <devcontactar@gmail.com>
2025-02-08 18:02:41 -0300

    Sortear amigo

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -15,5 +15,25 @@ function adicionarAmigo() {
 amigos.push(nome);
   atualizarListaAmigos();
   inputNome.value = "";
+  function atualizarListaAmigos() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    amigos.forEach((amigo, index) => {
+      lista.innerHTML += `<li class="name-item">${index + 1}. ${amigo}</li>`;
+    });
+  }
 
+  function sortearAmigo() {
+    if (amigos.length === 0) {
+      alert("Nenhum amigo disponível para sortear.");
+      return;
+    }
+  
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];
+    
+    const resultadoLista = document.getElementById("resultado");
+    resultadoLista.innerHTML = `<li class="result-item">Amigo Secreto Sorteado: <strong>${amigoSorteado}</strong></li>`;
+  }
+  
 
